cb644248b1503959b2dcadb6d6097711976f6fc7
unknown <adrian.reybrea@dxc.com>
2024-09-10 13:50:42 +0200

    Controlador accounts

--- src/main/java/com/bananaapps/MyOnlineShoppingService/domain/controllers/CustomerController.java
+++ src/main/java/com/bananaapps/MyOnlineShoppingService/domain/controllers/CustomerController.java
@@ -1,4 +1,7 @@
 package com.bananaapps.MyOnlineShoppingService.domain.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
 }
