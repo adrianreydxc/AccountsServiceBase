4dc3a52375388b63d7bca122e1794a4836551799
Carol <carolbrazalessempere@gmail.com>
2024-09-13 12:16:49 +0200

    Add validations

--- src/main/java/com/bananaapps/MyOnlineShoppingService/domain/entities/Customer.java
+++ src/main/java/com/bananaapps/MyOnlineShoppingService/domain/entities/Customer.java
@@ -6,6 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
@@ -17,6 +21,13 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "El nombre no puede ser null")
+    @NotEmpty(message = "El nombre es obligatorio, no puede estar vacío")
+    @Size(min = 2, max = 80, message = "Nombre entre 2 y 80 caracteres")
     private String name;
+
+    @NotNull(message = "El email no puede ser null")
+    @Email(message= "El email debe tener un formato váldio")
     private String email;
 }
