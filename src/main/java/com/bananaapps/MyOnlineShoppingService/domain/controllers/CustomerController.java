539d4c0e21d9821b6332b476550aa378a03bc056
unknown <adrian.reybrea@dxc.com>
2024-09-13 15:25:41 +0200

    CustomerController

--- src/main/java/com/bananaapps/MyOnlineShoppingService/domain/controllers/CustomerController.java
+++ src/main/java/com/bananaapps/MyOnlineShoppingService/domain/controllers/CustomerController.java
@@ -1,12 +1,14 @@
 package com.bananaapps.MyOnlineShoppingService.domain.controllers;
 
+import com.bananaapps.MyOnlineShoppingService.domain.dto.response.CustomerDto;
 import com.bananaapps.MyOnlineShoppingService.domain.entities.Customer;
 import com.bananaapps.MyOnlineShoppingService.domain.services.CustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,7 +21,20 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping()
-    public List<Customer> getCustomers() {
-        return List.of();
+    public ResponseEntity<List<CustomerDto>> getCustomers() {
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.getAllCustomers());
+    }
+    @PostMapping
+    public ResponseEntity<CustomerDto> newCustomer(@Valid @RequestBody CustomerDto customer) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customer));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerDto> editCustomer(@PathVariable("id") Long id, @Valid @RequestBody CustomerDto customer) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.updateCustomer(id, customer));
+    }
+    @DeleteMapping("/id")
+    public ResponseEntity<Object> deleteCustomer(@PathVariable("id") Long id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
     }
 }
