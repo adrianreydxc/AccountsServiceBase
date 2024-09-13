cf5a851737a1b58e4ab63e5b56f57e0d225465b9
unknown <adrian.reybrea@dxc.com>
2024-09-13 13:24:53 +0200

    Controladores actualizados

--- src/main/java/com/bananaapps/MyOnlineShoppingService/domain/config/InterceptorConfig.java
+++ src/main/java/com/bananaapps/MyOnlineShoppingService/domain/config/InterceptorConfig.java
@@ -14,6 +14,6 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(accountServiceInterceptor).addPathPatterns("/accounts");
+        //registry.addInterceptor(accountServiceInterceptor).addPathPatterns("/accounts");
     }
 }
