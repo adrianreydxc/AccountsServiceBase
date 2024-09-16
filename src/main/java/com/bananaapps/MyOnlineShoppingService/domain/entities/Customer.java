da707190dc1f6b2eed741e4ca84039c5019c20a0
Carol <carolbrazalessempere@gmail.com>
2024-09-16 12:12:45 +0200

    Model swagger documentation

--- src/main/java/com/bananaapps/MyOnlineShoppingService/domain/entities/Customer.java
+++ src/main/java/com/bananaapps/MyOnlineShoppingService/domain/entities/Customer.java
@@ -1,5 +1,6 @@
 package com.bananaapps.MyOnlineShoppingService.domain.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,14 +21,17 @@ import javax.validation.constraints.Size;
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(name = "Account ID", example = "1", required = true)
     private Long id;
 
     @NotNull(message = "El nombre no puede ser null")
     @NotEmpty(message = "El nombre es obligatorio, no puede estar vacío")
     @Size(min = 2, max = 80, message = "Nombre entre 2 y 80 caracteres")
+    @Schema(name = "Customer's name", example = "John Doe", required = true)
     private String name;
 
     @NotNull(message = "El email no puede ser null")
     @Email(message= "El email debe tener un formato váldio")
+    @Schema(name = "Customer's email", example = "johndoe@email.com", required = true)
     private String email;
 }
