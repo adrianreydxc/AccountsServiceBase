3ce3aef2e94a4767e8862114d235a83697c74900
Adrián <adroloja@gmail.com>
2024-09-17 16:58:31 +0200

    Mock test

---
+++ src/test/java/com/bananaapps/MyOnlineShoppingService/repository/AccountRepositoryMockTest.java
@@ -0,0 +1,40 @@
+package com.bananaapps.MyOnlineShoppingService.repository;
+
+import com.bananaapps.MyOnlineShoppingService.domain.repositories.AccountServiceRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AccountRepositoryMockTest {
+
+    @Mock
+    private AccountServiceRepository accountServiceRepository;
+
+    @BeforeEach
+    void setup(){
+
+    }
+
+    @Test
+    void getAccountByUser_AllOk(){
+
+    }
+
+    @Test
+    void getAccountByUser_WrongId(){
+
+    }
+
+    @Test
+    void deleteAllAccountsByUser_AllOk(){
+
+    }
+
+    @Test
+    void deleteAllAccountsByUser_WrongId(){
+
+    }
+}
--- src/test/java/com/bananaapps/MyOnlineShoppingService/service/AccountServiceMockTest.java
+++ src/test/java/com/bananaapps/MyOnlineShoppingService/service/AccountServiceMockTest.java
@@ -1,19 +1,25 @@
 package com.bananaapps.MyOnlineShoppingService.service;
 
 import com.bananaapps.MyOnlineShoppingService.domain.dto.response.AccountDto;
-import com.bananaapps.MyOnlineShoppingService.domain.entities.Account;
 import com.bananaapps.MyOnlineShoppingService.domain.services.AccountService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 @SpringBootTest
-public class AccountServiceIntegrationTest {
+public class AccountServiceMockTest {
 
-    @Autowired
+    @Mock
     private AccountService accountService;
+
+    @BeforeEach
+    void setup(){
+
+    }
 
     @Test
     void getAllAccounts_OK() throws Exception {
