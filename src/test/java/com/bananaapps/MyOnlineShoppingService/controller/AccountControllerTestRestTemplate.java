6cba6a2278eec1b07c2089c5261c4987e70a088b
Carol <carolbrazalessempere@gmail.com>
2024-09-18 10:21:50 +0200

    Add test check loan

--- src/test/java/com/bananaapps/MyOnlineShoppingService/controller/AccountControllerTestRestTemplate.java
+++ src/test/java/com/bananaapps/MyOnlineShoppingService/controller/AccountControllerTestRestTemplate.java
@@ -1,6 +1,7 @@
 package com.bananaapps.MyOnlineShoppingService.controller;
 
 
+import com.bananaapps.MyOnlineShoppingService.domain.dto.request.LoanDto;
 import com.bananaapps.MyOnlineShoppingService.domain.dto.request.MoneyTransactionsDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,47 @@ public class AccountControllerTestRestTemplate {
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.CREATED)));
         assertThat(response.getBody(), is(true));
     }
+
+    @Test
+    public void givenValidRequest_whenWithdrawMoney_thenReturnOkAndTrue() {
+        // Given
+        MoneyTransactionsDto request = new MoneyTransactionsDto();
+        request.setIdAccount(1L);
+        request.setIdUser(1L);
+        request.setAmount(50.0);
+
+        String url = "http://localhost:" + port + "/accounts/withdrawMoney";
+
+        HttpEntity<MoneyTransactionsDto> entity = new HttpEntity<>(request);
+
+        // When
+        ResponseEntity<Boolean> response = restTemplate.exchange(url, HttpMethod.PUT, entity, Boolean.class);
+
+        // Then
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatus.CREATED)));
+        assertThat(response.getBody(), is(true));
+    }
+
+    @Test
+    public void givenValidUserAndLoanDto_whenCheckLoan_thenReturnOkAndTrue() {
+        // Given:
+        Long userId = 1L;
+        LoanDto loanDto = LoanDto.builder()
+                .amount(1000.0)
+                .build();
+
+        String url = "http://localhost:" + port + "/accounts/loan/" + userId;
+
+        HttpEntity<LoanDto> entity = new HttpEntity<>(loanDto);
+
+        // When: the request is made to check loan eligibility
+        ResponseEntity<Boolean> response = restTemplate.exchange(url, HttpMethod.POST, entity, Boolean.class);
+
+        // Then: the response status should be OK and the body should be true
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatus.OK)));
+        assertThat(response.getBody(), is(true));
+    }
+
 
 
 }
