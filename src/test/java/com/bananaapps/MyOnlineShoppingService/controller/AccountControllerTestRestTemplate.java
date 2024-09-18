0d9f1199e93e8e72743b1f91a61b7d3f85bc7483
Carol <carolbrazalessempere@gmail.com>
2024-09-18 10:24:17 +0200

    Minor changes

--- src/test/java/com/bananaapps/MyOnlineShoppingService/controller/AccountControllerTestRestTemplate.java
+++ src/test/java/com/bananaapps/MyOnlineShoppingService/controller/AccountControllerTestRestTemplate.java
@@ -80,10 +80,10 @@ public class AccountControllerTestRestTemplate {
 
         HttpEntity<LoanDto> entity = new HttpEntity<>(loanDto);
 
-        // When: the request is made to check loan eligibility
+        // When
         ResponseEntity<Boolean> response = restTemplate.exchange(url, HttpMethod.POST, entity, Boolean.class);
 
-        // Then: the response status should be OK and the body should be true
+        // Then
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.OK)));
         assertThat(response.getBody(), is(true));
     }
